7176cde34004b113b33debef8c62fc50bbcfa545
Antonio <cuentadisponible89@gmail.com>
2022-04-14 00:59:45 +0200

    Test actualizado.

--- test/tests.js
+++ test/tests.js
@@ -3,9 +3,9 @@ let server = require('../server.js');
 
 describe('Tests unitarios', function(){
     describe('Longitud correcta de la fecha completa:', function(){
-        it('28:', function(done){
+        it('42:', function(done){
             let f = server.getFechaCompleta();
-            expect(f.length).equal(28);
+            expect(f.length).equal(42);
             done();
         });
     });
